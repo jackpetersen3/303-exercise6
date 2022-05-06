f991ec2dab10bfc2c6968ae1e8f1ce5e25288142
Jack Petersen <jack.petersen3@gmail.com>
2022-05-06 13:58:00 -0700

    Apply Extract Function to create two seperate function

--- main.rb
+++ main.rb
@@ -1,11 +1,10 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-    if is_dark_mode
-      # darken foreground color for dark mode
-      paint(label_text, x, y, foreground_color - 10, '#111111')
-    else
-      # lighten foreground color for non-dark mode
-      paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
-    end
+
+  def draw_button_light_mode(label_text, x, y, foreground_color, is_dark_mode)
+    # lighten foreground color for non-dark mode
+    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
   end
 
-  
+  def draw_button_dark_mode(label_text, x, y, foreground_color, is_dark_mode)
+    # darken foreground color for dark mode
+    paint(label_text, x, y, foreground_color - 10, '#111111')
+  end
