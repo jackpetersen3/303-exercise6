a42557afa0c58b02f5c17eb4a53ddaecbe0cb7dc
Jack Petersen <jack.petersen3@gmail.com>
2022-05-06 14:05:42 -0700

    Apply introduce parameter object to address long parameter list

--- main.rb
+++ main.rb
@@ -1,8 +1,17 @@
-
-  def draw_button_light_mode(label_text, x, y, foreground_color)
+class Button 
+    def initialize
+        @label_text = label_text
+        @x = x
+        @y = y
+        @foreground_color = foreground_color
+    end
+end
+  
+   
+def draw_button_light_mode(label_text, x, y, foreground_color)
     paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
-  end
+ end
 
-  def draw_button_dark_mode(label_text, x, y, foreground_color)
+def draw_button_dark_mode(label_text, x, y, foreground_color)
     paint(label_text, x, y, foreground_color - 10, '#111111')
-  end
+end
