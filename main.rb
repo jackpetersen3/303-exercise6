d7589174dce85ce2bf0ada73cd7e9b7da09849eb
Jack Petersen <jack.petersen3@gmail.com>
2022-05-06 14:08:07 -0700

    Remove unneeded parameters from functions in class

--- main.rb
+++ main.rb
@@ -6,11 +6,11 @@ class Button
         @foreground_color = foreground_color
     end
 
-    def draw_button_light_mode(label_text, x, y, foreground_color)
+    def draw_button_light_mode
         paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
      end
     
-    def draw_button_dark_mode(label_text, x, y, foreground_color)
+    def draw_button_dark_mode
         paint(label_text, x, y, foreground_color - 10, '#111111')
     end
     
