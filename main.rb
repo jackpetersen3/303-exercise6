432ae352887b059d201568219c165e93c27a79d4
Jack Petersen <jack.petersen3@gmail.com>
2022-05-06 14:13:17 -0700

    Extract foreground color into function

--- main.rb
+++ main.rb
@@ -7,11 +7,19 @@ class Button
     end
 
     def draw_button_light_mode
-        paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+        paint(label_text, x, y, light_foreground_color)
      end
     
     def draw_button_dark_mode
-        paint(label_text, x, y, foreground_color - 10, '#111111')
+        paint(label_text, x, y, dark_foreground_color)
+    end
+
+    def light_foreground_color
+        foreground_color + 10, '#E0E0E0'
+    end
+
+    def dark_foreground_color
+        foreground_color - 10, '#111111'
     end
     
 end
