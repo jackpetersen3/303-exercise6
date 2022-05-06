8124126222255f726240d670e22dcb39e86d2479
Jack Petersen <jack.petersen3@gmail.com>
2022-05-06 14:07:37 -0700

    Apply Combine Functions into Class

--- main.rb
+++ main.rb
@@ -5,13 +5,14 @@ class Button
         @y = y
         @foreground_color = foreground_color
     end
+
+    def draw_button_light_mode(label_text, x, y, foreground_color)
+        paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+     end
+    
+    def draw_button_dark_mode(label_text, x, y, foreground_color)
+        paint(label_text, x, y, foreground_color - 10, '#111111')
+    end
+    
 end
   
-   
-def draw_button_light_mode(label_text, x, y, foreground_color)
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
- end
-
-def draw_button_dark_mode(label_text, x, y, foreground_color)
-    paint(label_text, x, y, foreground_color - 10, '#111111')
-end
